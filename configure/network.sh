6c98079359ae0383d695c88d8e1d4b73e19b8cff
UnluckyTech <stawse99@protonmail.com>
2024-01-21 17:12:29 -0500

    Update network.sh

--- configure/network.sh
+++ configure/network.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-Echo "Specify your desired hostname"
+echo "Specify your desired hostname"
 read hostname
 
 # Use echo to add the hostname to /etc/hostname
-echo "$hostname" | sudo tee /etc/hostname > /dev/null
+echo "$hostname" | tee /etc/hostname > /dev/null
 
 echo "Hostname set to $hostname."
 sleep 2
 systemctl enable fstrim.timer
 
 pacman_conf="/etc/pacman.conf"
-section="[multilib]"
-sed -i "/^# $section/,/^#$/ s/^#//" "$pacman_conf"
+section="#[multilib]"
+mirror="#Include = /etc/pacman.d/mirrorlist"
+newmirror="Include = /etc/pacman.d/mirrorlist"
+
+sed -i "" "s/$section/[multilib]/" $pacman_conf
+sed -i "" "s/$mirror/$newmirror/" $pacman_conf
 
 echo "Multilib section uncommented in $pacman_conf."
 echo "Updating..."
