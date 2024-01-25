654eceb5d9f83e3432c7d5d93350144d2a5b7012
UnluckyTech <stawse99@protonmail.com>
2024-01-25 15:03:26 -0500

    added option to adjust root partition size

--- prepare/partition.sh
+++ prepare/partition.sh
@@ -11,18 +11,36 @@ if [[ $erase == "y" ]]; then
     # Create EFI system (assuming 1GiB size), swap partition (user-defined size), root partition with remaining space.
     echo "How much storage in GB would you like on swap?"
     read swapgb
-    ( echo 'g' ; echo 'n' ; echo '1' ; echo '' ; echo '+1G' ; echo 't' ; echo '1' ; echo 'n' ; echo '2' ; echo '' ; echo "+${swapgb}G" ; echo 't' ; echo '2' ; echo '19' ; echo 'n' ; echo '3' ; echo '' ; echo '' ; echo 'w' ) | fdisk "$device"
+    echo "Use remaining space for root partition? [y/n]"
+    read rootsize
+        if [[ $rootsize == "y" ]]; then
+            ( echo 'g' ; echo 'n' ; echo '1' ; echo '' ; echo '+1G' ; echo 't' ; echo '1' ; echo 'n' ; echo '2' ; echo '' ; echo "+${swapgb}G" ; echo 't' ; echo '2' ; echo '19' ; echo 'n' ; echo '3' ; echo '' ; echo '' ; echo 'w' ) | fdisk "$device"
 
-    # Format partitions
-    mkfs.fat -F32 "${device}1"  # Format EFI partition
-    mkfs.ext4 "${device}3"      # Format root partition
-    mkswap "${device}2"         # Format swap partition
+            # Format partitions
+            mkfs.fat -F32 "${device}1"  # Format EFI partition
+            mkfs.ext4 "${device}3"      # Format root partition
+            mkswap "${device}2"         # Format swap partition
 
-    # Display partition information
-    fdisk -l
-    echo "For drive $device, you should see 3 partitions."
-    echo "Returning to menu"
-    sleep 2
+            # Display partition information
+            fdisk -l
+            echo "For drive $device, you should see 3 partitions."
+            echo "Returning to menu"
+            sleep 2
+        elif [[ $rootsize == "n" ]]; then
+            echo "Set root partition size"
+            read partsize
+            ( echo 'g' ; echo 'n' ; echo '1' ; echo '' ; echo '+1G' ; echo 't' ; echo '1' ; echo 'n' ; echo '2' ; echo '' ; echo "+${swapgb}G" ; echo 't' ; echo '2' ; echo '19' ; echo 'n' ; echo '3' ; echo '' ; echo "+${partsize}G" ; echo 'w' ) | fdisk "$device"
+
+            # Format partitions
+            mkfs.fat -F32 "${device}1"  # Format EFI partition
+            mkfs.ext4 "${device}3"      # Format root partition
+            mkswap "${device}2"         # Format swap partition
+
+            # Display partition information
+            fdisk -l
+            echo "For drive $device, you should see 3 partitions."
+            echo "Returning to menu"
+            sleep 2
 elif [[ $erase == "n" ]]; then
     echo "Returning to Menu"
 else
