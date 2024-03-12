3b4b9ac7bd4aa506499f278be353bf8cb56f0c9d
UnluckyTech <stawse99@protonmail.com>
2024-03-12 15:21:59 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,11 +4,11 @@ read user_device
 echo "Editing boot config..."
 refind_linux_conf="/boot/refind_linux.conf"
 
-# Define the content
+# Define the content with the ${user_device} variable
 content='
-"Boot with standard options"  "rw root=${user_device}"
-"Boot to single-user mode"    "rw root=${user_device} single"
-"Boot with minimal options"   "ro root=${user_device}"
+"Boot with standard options"  "rw root='"${user_device}"'"
+"Boot to single-user mode"    "rw root='"${user_device}"' single"
+"Boot with minimal options"   "ro root='"${user_device}"'"
 '
 
 # Use echo to create the file with the specified content
