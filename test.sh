3d1cf2cff451e8b221f50b9df0916a9d7ee48c2d
UnluckyTech <stawse99@protonmail.com>
2024-03-12 15:13:34 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,51 +1,12 @@
 #!/bin/bash
-
-get_partition_syntax() {
-    local device=$1
-
-    if [[ $device == /dev/nvme* ]]; then
-        echo "${device}p"
-    else
-        echo "${device}"
-    fi
-}
-
-while true
-do
-
-    echo ''
-    echo '*********************************'
-    echo '*********   Boot Loader   *******'
-    echo '*********************************'
-    echo '1. rEFInd'
-    echo '2. GRUB'
-    echo '3. Exit'
-    dir=$(pwd)
-    user=$(whoami)
-    read option
-    if [[ $option == "1" ]]; then
-        
-        read device
-        user_device=$(get_partition_syntax "$device")
-        echo "${user_device}"
-
-        sleep 10
-        if [[ $option == "y" ]]; then
-           echo ""
-        elif [[ $option == "n" ]]; then
-            echo ""
-        fi
-        echo "Done! Hopefully it works!"
-
-    elif [[ $option == "2" ]]; then
-        echo "No idea yet"
-
-    elif [[ $option == "3" ]]; then
-        break
-    else
-        2>/dev/null
-        echo 'Incorrect command. Try again.'
-    fi
-
-done
-
+echo "input device"
+read user_device
+echo "Editing boot config..."
+content='"Boot with minimal options" "rw root='"${user_device}3"'"'
+# Specify the file path
+refind_linux_conf="/boot/refind_linux.conf"
+# Use sed to replace "ro" with "rw" in the file content
+sudo sed -i 's/ro/rw/' "$refind_linux_conf"
+# Use echo to create/replace the file with the modified content
+echo "$content" | sudo tee "$refind_linux_conf" > /dev/null
+echo "File replaced: $refind_linux_conf"
